114354c568055cdefcfabdafced93de500876290
DanielKrs1 <krstndaniel24@gmail.com>
2023-10-17 21:41:13 -0400

    commit

--- sectionChange.js
+++ sectionChange.js
@@ -1,15 +1,23 @@
+let sectionDivs = document.getElementsByName("section");
 let sectionDropdown = document.getElementById("section dropdown");
+let categoryImage = document.getElementById("category image");
+
+let divImages = {
+    "introduction" : "images/sisleyDippinDots.jpg",
+    "what she likes" : "images/sisleyHappy.jpg",
+    "what she doesn't like" : "images/sisleySad.jpg",
+}
 
 let onSectionChange = function() {
-    let sectionDivs = document.getElementsByName("section");
-
-    for (let i = 0; i < sectionDivs.length; i++) {
-        sectionDivs[i].style.display = "none";
+    for (let sectionDiv of sectionDivs) {
+        sectionDiv.style.display = "none";
     }
 
     let selectedSection = sectionDropdown.value;
     let selectedSectionDiv = document.getElementById(selectedSection);
     selectedSectionDiv.style.display = "block";
+
+    categoryImage.src = divImages[selectedSectionDiv.id];
 }
 
 sectionDropdown.addEventListener("change", onSectionChange);
